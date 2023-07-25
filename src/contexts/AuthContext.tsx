3c4508abc2f6f11673dfbff669c6ac484a9322a2
mtsoft936 <mtsoft0936@gmail.com>
2023-07-25 05:08:18 -0500

    login alert

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -34,6 +34,9 @@ export function AuthProvider({ children }: { children: JSX.Element }) {
   const [token, setToken] = useState(Cookies.get("token") || null);
   const [error, setError] = useState("");
 
+  const [alertshow, Setalertshow] = useState(false);
+  const [alertcontent, Setalertcontent] = useState("");
+
   const signup = async (formData: registerData) => {
     try {
       const result = await register(formData);
@@ -48,12 +51,13 @@ export function AuthProvider({ children }: { children: JSX.Element }) {
     try {
       const result = await login(formData);
       console.log(result);
-      if(result.token) {
+      if (result.token) {
         setAccount(JSON.stringify(result.user));
         setToken(result.token);
       }
-      else{
-        alert("Your email or password is incorrect!")
+      else {
+        Setalertcontent("Your email or password is incorrect!");
+        Setalertshow(true);
       }
     } catch (e) {
       setError((e as Error).message);
@@ -71,7 +75,21 @@ export function AuthProvider({ children }: { children: JSX.Element }) {
       logout();
     }
   }
-  
+
+  const Alert = () => {
+    return (
+      <div role="alert" className="w-[50%] mx-auto mt-10 absolute ml-[25%] ">
+        <div className="border border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700 flex justify-between">
+          <div>{alertcontent}</div>
+          <button onClick={() =>{Setalertshow(false)}}>
+            <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+          </button>
+        </div>
+
+      </div>
+    )
+  }
+
   setInterval(checkCookieExpiration, 60000);
 
   const getAccount = async () => {
@@ -121,6 +139,7 @@ export function AuthProvider({ children }: { children: JSX.Element }) {
         logout,
       }}
     >
+      {alertshow&&<Alert />}
       {children}
     </AuthContext.Provider>
   );
